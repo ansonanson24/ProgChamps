08b70eacdc5f4e734a0660c8cf3c0bd49611a56a
Bilal Ali <bilal.ali2571@gmail.com>
2019-09-23 16:26:22 +1000

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
  * Bilal Ali
  * Anson Kwok
  * Danielle Alota
- * Zhongzhuo Wu
+ * Jessie Wu
  * Yuekai Sun
  *
  * Student ID:
@@ -48,6 +48,14 @@ typedef struct person person_t;
 /*******************************************************************************
  * Function prototypes.
 *******************************************************************************/
+void printMain();
+void selectionMain();
+void adminLogin();
+void adminMenu();
+void printAdmin();
+void selectionAdmin();
+void userLogin();
+void userRegister();
 void addMember();
 void removeMember();
 void assignMembers();
@@ -60,17 +68,141 @@ void printList();
  * Main
 *******************************************************************************/
 int main(void) {
+	
 	person_t persons[MAX_NUM][MAX_NUM];
-	/* subject to change, 2d array was discussed as a possibility*/
+	/*subject to change, 2d array was discussed as a possibility*/
+	
+	while(1) {
+	printMain();
+	selectionMain();
+	}
 
 	return 0;
+}
+
+void printMain() {
+	printf("\n"
+    "1. Login as Admin\n"
+    "2. Login as User\n"
+    "3. Register\n"
+    "4. Exit the program\n"
+    "Enter choice (number between 1-4)>\n");
+}
+
+void selectionMain() {
+	int number;
+	scanf("%d", &number);
+	
+	switch(number)
+	{
+		case 1:
+		adminLogin();
+		break;
+		
+		case 2:
+		userLogin();
+		break;
+		
+		case 3:
+		userRegister();
+		break;
+		
+		case 4:
+		exit(0);
+		break;
+		
+		default:
+		printf("Invalid choice\n");
+		break;
+	}
+}
+
+void adminLogin() {
+	char username[MAX_NAME_LEN];
+	char password[MAX_PASS_LEN];
+
+	printf("Enter username>\n");
+	scanf("%s", username);
+
+	printf("Enter password>\n");
+	scanf("%s", password);
+	
+	if(strcmp(username, "admin") == 0 && strcmp(password, "admin") == 0) {
+		printf("Login success\n");
+		adminMenu();
+	} else {
+	printf("Login failed\n");
+	return;
+	}
+}
+
+void adminMenu() {
+	while(1) {
+	printAdmin();
+	selectionAdmin();
+	}
+}
+
+void printAdmin() {
+	printf("\n"
+    "1. Display users\n"
+    "2. Remove users\n"
+    "3. Assign users\n"
+    "4. Log out\n"
+    "5. Exit the program\n"
+    "Enter choice (number between 1-4)>\n");
+}
+
+void selectionAdmin() {
+	int number;
+	scanf("%d", &number);
+	
+	switch(number)
+	{
+		case 1:
+		printf("displaying...\n");
+		return;
+		break;
+		
+		case 2:
+		printf("removing...\n");
+		return;
+		break;
+		
+		case 3:
+		printf("assigning...\n");
+		return;
+		break;
+
+		case 4:
+		main();
+		return;
+		break;		
+
+		case 5:
+		exit(0);
+		break;
+		
+		default:
+		printf("Invalid choice\n");
+		break;
+	}
+}
+
+void userLogin() {
+
+}
+
+
+void userRegister() {
+	
 }
 
 /*******************************************************************************
 *	This function adds a new member inputted by the user to the member list.
 *******************************************************************************/
 void addMember() {
-	
+	printf("add\n");
 }
 
 /*******************************************************************************
@@ -78,42 +210,42 @@ void addMember() {
 *	member list.
 *******************************************************************************/
 void removeMember() {
-
+	printf("remove\n");
 }
 
 /*******************************************************************************
 *	This function assigns a random member's wishlist to another member.
 *******************************************************************************/
 void assignMembers() {
-
+	printf("assign\n");
 }
 
 /*******************************************************************************
 *	This function handles the displaying of a member's wishlist.
 *******************************************************************************/
-void viewWishes() {
-
+void viewWishes() {	
+	printf("view\n");
 }
 
 /*******************************************************************************
 *	This function encrypts a given password.
 *******************************************************************************/
 void passEncrypt() {
-
+	printf("encrypt\n");
 }
 
 /*******************************************************************************
 *	This function sorts the member list by their names in alphabetical order.
 *******************************************************************************/
 void sortByAlphabet() {
-
+	printf("sort\n");
 }
 
 /*******************************************************************************
 *	This function prints a given wishlist.
 *******************************************************************************/
 void printList() {
-
+	printf("print\n");
 }
 
 
